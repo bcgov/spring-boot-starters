3c95407a97b24e93d6ee9af32820a3a2ceb7b3e5
TayGov <Taylor.Clausen@gov.bc.ca>
2020-08-13 09:44:25 -0700

    rename

--- src/spring-sftp-starter/src/main/java/ca/bc/gov/open/sftp/starter/SftpProperties.java
+++ src/spring-sftp-starter/src/main/java/ca/bc/gov/open/sftp/starter/SftpProperties.java
@@ -7,7 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  *
  * @author alexjoybc@github
  */
-@ConfigurationProperties(prefix = "starter.sftp")
+@ConfigurationProperties(prefix = "bcgov.sftp")
 public class SftpProperties {
 
     private String host;
