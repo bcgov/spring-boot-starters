80e6179453d723864b72bf3fe96cd9aeb78fc8e4
TayGov <Taylor.Clausen@gov.bc.ca>
2020-08-17 10:25:33 -0700

    Add test for mapper bean

--- src/spring-bceid-starter/src/test/java/ca/bc/gov/open/bceid/starter/AutoConfigurationTest.java
+++ src/spring-bceid-starter/src/test/java/ca/bc/gov/open/bceid/starter/AutoConfigurationTest.java
@@ -1,6 +1,7 @@
 package ca.bc.gov.open.bceid.starter;
 
 import ca.bc.gov.open.bceid.starter.account.BCeIDAccountService;
+import ca.bc.gov.open.bceid.starter.account.mappers.AccountDetailRequestMapper;
 import ca.bceid.webservices.client.v9.BCeIDServiceSoap;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -19,6 +20,7 @@ public class AutoConfigurationTest {
     public void testConfigure() {
 
         context.run(it -> {
+            Assertions.assertNotNull(assertThat(it).getBean(AccountDetailRequestMapper.class));
             Assertions.assertNotNull(assertThat(it).getBean(BCeIDServiceSoap.class));
             Assertions.assertNotNull(assertThat(it).getBean(BCeIDAccountService.class));
         });
