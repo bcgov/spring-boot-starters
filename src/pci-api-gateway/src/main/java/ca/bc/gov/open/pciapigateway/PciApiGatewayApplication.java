1d32909c99e6066e111fa922b6d41b5c33d53280
taylorclausen <taylor.clausen@nttdata.com>
2020-11-12 08:53:28 -0800

    Initial commit

---
+++ src/pci-api-gateway/src/main/java/ca/bc/gov/open/pciapigateway/PciApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.open.pciapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PciApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PciApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/pci-api-gateway/src/test/java/ca/bc/gov/open/pciapigateway/PciApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.open.pciapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PciApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
